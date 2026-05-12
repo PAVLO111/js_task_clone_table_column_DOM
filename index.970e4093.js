7d1d62775e0cc73de4d9a3285088ce294ec900bc
pavlo <nadazasada@gmail.com>
2026-05-12 21:23:43 +0300

    make build

---
+++ index.970e4093.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("table tr").forEach(function(e){var t=e.cells[1];if(t){var n=document.createElement(t.tagName);n.textContent=t.textContent;var l=e.lastElementChild;e.insertBefore(n,l)}});
+//# sourceMappingURL=index.970e4093.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+function cloneColumn(indexToClone) {
+    const rows = document.querySelectorAll("table tr");
+    rows.forEach((row)=>{
+        const targetCell = row.cells[indexToClone];
+        if (targetCell) {
+            const newCell = document.createElement(targetCell.tagName);
+            newCell.textContent = targetCell.textContent;
+            const lastCell = row.lastElementChild;
+            row.insertBefore(newCell, lastCell);
+        }
+    });
+}
+cloneColumn(1);
+
+//# sourceMappingURL=index.f75de5e1.js.map
